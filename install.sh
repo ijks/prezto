de2f4b67f1fb928396aafa1289c4ace6a330649c
Daan Rijks <daanrijks@gmail.com>
2015-06-28 20:38:22 +0200

    Reset install.sh to initial version.
    To install using GNU Stow, see `symlink.sh` in my dotfiles repo under zsh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 setopt EXTENDED_GLOB
 for rcfile in runcoms/^README.md(.N); do
     full_path=$(readlink -e "$rcfile")
-    ln -vs "$full_path" "../.${rcfile:t}"
+    ln -s "$full_path" "../.${rcfile:t}"
 done
-ln -vs $(readlink -e .) ../.zprezto
 unsetopt EXTENDED_GLOB
