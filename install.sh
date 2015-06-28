f637c134ac4a3cfde27c6ba9cc69c40c371828e8
Daan Rijks <daanrijks@gmail.com>
2015-06-28 18:52:38 +0200

    Make install script link .zprezto directory as well

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 setopt EXTENDED_GLOB
 for rcfile in runcoms/^README.md(.N); do
     full_path=$(readlink -e "$rcfile")
-    ln -s "$full_path" "../.${rcfile:t}"
+    ln -vs "$full_path" "../.${rcfile:t}"
 done
+ln -vs $(readlink -e .) ../.zprezto
 unsetopt EXTENDED_GLOB
